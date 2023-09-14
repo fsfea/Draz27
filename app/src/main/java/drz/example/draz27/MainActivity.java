e0801e9e7127c5ce0acaffd80875253a3258e655
Admin <WIPES.6098@GMAIL.COM>
2023-09-14 13:53:13 +0300

    Initial commit

--- app/src/main/java/drz/example/draz27/MainActivity.java
+++ app/src/main/java/drz/example/draz27/MainActivity.java
@@ -3,6 +3,8 @@ package drz.example.draz27;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("draz","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("draz","ez");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
     }
 }
