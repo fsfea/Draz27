ed4b18a4c0e78380fdc650db5e07ad8988bc2e02
Admin <WIPES.6098@GMAIL.COM>
2023-09-19 14:59:23 +0300

    Initial commit

---
+++ app/src/main/java/drz/example/draz27/SignInMainActivity2.java
@@ -0,0 +1,14 @@
+package drz.example.draz27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInMainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_main2);
+    }
+}
---
+++ app/src/main/java/drz/example/draz27/Sign_InMainActivity2.java
@@ -0,0 +1,14 @@
+package drz.example.draz27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_InMainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_main2);
+    }
+}
