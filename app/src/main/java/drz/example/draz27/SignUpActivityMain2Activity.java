63372265c3f350cf2dd010ec09fc103ae8644648
Admin <WIPES.6098@GMAIL.COM>
2023-09-24 09:40:35 +0300

    Initial commit

---
+++ app/src/main/java/drz/example/draz27/SignUpActivityMain2Activity.java
@@ -0,0 +1,14 @@
+package drz.example.draz27;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivityMain2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_activity_main2);
+    }
+}
