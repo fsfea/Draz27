6bdeac772ee6a7f9f9dd7a7d0544eeb68350067c
Admin <WIPES.6098@GMAIL.COM>
2023-10-03 12:21:05 +0300

    Initial commit

--- app/src/main/java/drz/example/draz27/data/AppDatabase.java
+++ app/src/main/java/drz/example/draz27/data/AppDatabase.java
@@ -7,25 +7,26 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import drz.example.draz27.data.SubjectTable.MySubject;
+import drz.example.draz27.data.SubjectTable.MySubjectQuery;
 import drz.example.draz27.data.mytasksTable.MyTasks;
 import drz.example.draz27.data.mytasksTable.MyTasksQuery;
 import drz.example.draz27.data.usersTable.MyUser;
 import drz.example.draz27.data.usersTable.MyuserQuery;
 
-public class appDataBase
-{
+
         @Database(entities = {MyUser.class, MySubject.class, MyTasks.class},version = 5)
-        public abstract class AppDatabase extends RoomDatabase
-        {
+        public abstract class AppDatabase extends RoomDatabase {
                 private static AppDatabase db;
+
                 public abstract MyuserQuery.MyUserQuery getMyUserQuery();
+
                 public abstract MySubjectQuery getMySubjectQuery();
+
                 public abstract MyTasksQuery getMyTaskQuery();
 
-                public static AppDatabase getDB(Context context)
-                {
-                        if(db==null)
-                        {
+                public static AppDatabase getDB(Context context) {
+                        if (db == null) {
                                 db = Room.databaseBuilder(context,
                                                 AppDatabase.class, "database-name")
                                         .fallbackToDestructiveMigration()
@@ -35,5 +36,5 @@ public class appDataBase
                         return db;
 
 
-
                 }
+        }
