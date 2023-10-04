830db2cded010331c704b58f53ba7ed35ba26596
Admin <WIPES.6098@GMAIL.COM>
2023-10-04 10:48:01 +0300

    Initial commit

--- app/src/main/java/drz/example/draz27/data/AppDatabase.java
+++ app/src/main/java/drz/example/draz27/data/AppDatabase.java
@@ -13,18 +13,57 @@ import drz.example.draz27.data.mytasksTable.MyTasks;
 import drz.example.draz27.data.mytasksTable.MyTasksQuery;
 import drz.example.draz27.data.usersTable.MyUser;
 import drz.example.draz27.data.usersTable.MyuserQuery;
+/*
+تعريف الجداول ورقم الاصدار
+version
+عند تغيير اي شيء يخص الجداول او دوال علينا تغيير رقم الاصدار
+ليتم بناء قاعدة البيانات من جديد
+ */
+
 
 
         @Database(entities = {MyUser.class, MySubject.class, MyTasks.class},version = 5)
-        public abstract class AppDatabase extends RoomDatabase
+        /**
+         * الفئة المسؤولة عن بناء قاعدة البيانات بكل جداولها
+         * وتوفر لنا كائن للتعمل مع قاعدة البيانات
+         */
+
+
+
+public abstract class AppDatabase extends RoomDatabase
         {
+                /**
+                 * كائن للتعامل مع قاعدة البيانات
+                 */
                 private static AppDatabase db;
+
+                /**
+                 * يعيد كائن لعمليات جدول المستعملين
+                 * @return
+                 */
 
                 public abstract MyuserQuery.MyUserQuery getMyUserQuery();
 
+                /**
+                 *  يعيد كائن لعمليات جدول الموضيع
+                 * @return
+                 */
+
+
                 public abstract MySubjectQuery getMySubjectQuery();
 
+                /**
+                 *  يعيد كائن لعمليات جدول المهمات
+                 * @return
+                 */
+
                 public abstract MyTasksQuery getMyTaskQuery();
+
+                /**
+                 * بناء قاعدة البيانات واعادة كائن يؤشر عليها
+                 * @param context
+                 * @return
+                 */
 
                 public static AppDatabase getDB(Context context) {
                         if (db == null) {
