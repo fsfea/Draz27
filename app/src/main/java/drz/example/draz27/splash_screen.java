67f91e1d6cea336ca28676570b1dbed773991b89
Admin <WIPES.6098@GMAIL.COM>
2023-10-29 12:21:33 +0200

    Initial commit

--- app/src/main/java/drz/example/draz27/splash_screen.java
+++ app/src/main/java/drz/example/draz27/splash_screen.java
@@ -2,7 +2,9 @@ package drz.example.draz27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash_screen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //start next activity (screen) automatically  after period of time
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
+            @Override
+            public void run()
+            {
+                //to open new activity from current to next activity
+                Intent i= new Intent(splash_screen.this,  Sign_InMainActivity2.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r,3000);
     }
-}
+
+    }
+
