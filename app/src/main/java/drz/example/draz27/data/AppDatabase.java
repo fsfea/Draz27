b80ef90cbac9caa09d2344be95ce8a039fd9eeae
Admin <WIPES.6098@GMAIL.COM>
2024-01-15 09:41:06 +0200

    Initial commit

--- app/src/main/java/drz/example/draz27/data/AppDatabase.java
+++ app/src/main/java/drz/example/draz27/data/AppDatabase.java
@@ -7,54 +7,48 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import Draz.afinal.data.MyMessage.MyMessages;
+
 import drz.example.draz27.data.SubjectTable.MySubjectQuery;
+import drz.example.draz27.data.mytasksTable.MyTasks;
 import drz.example.draz27.data.mytasksTable.MyTasksQuery;
 import drz.example.draz27.data.usersTable.MyUser;
 import drz.example.draz27.data.usersTable.MyuserQuery;
 /*
 تعريف الجداول ورقم الاصدار
 version
-عند تغيير اي شيء يخص الجداول او دوال علينا تغيير رقم الاصدار
+عند تغيير اي شي يخص جدول او جداول علينا تغيير رقم الاصدار
 ليتم بناء قاعدة البيانات من جديد
- */
-
-
-@Database(entities = {MyUser.class,  MyMessages.class},version = 6)
+*/
+@Database(entities = {MyUser.class, MySubject.class, MyTasks.class},version =2)
 /**
- * الفئة المسؤولة عن بناء قاعدة البيانات بكل جداولها
- * وتوفر لنا كائن للتعمل مع قاعدة البيانات
+ * الفئة المسؤولة عن بناء قاعدة البايانات بكل جداولها
+ * وتوفر لنا كائن للتعامل مع قاعدة البيانات
  */
 
-
-
-public abstract class AppDatabase extends RoomDatabase
+public abstract class  AppDataBase extends RoomDatabase
 {
+
         /**
          * كائن للتعامل مع قاعدة البيانات
          */
-        private static AppDatabase db;
+        private static AppDataBase db;
 
         /**
          * يعيد كائن لعمليات جدول المستعملين
          * @return
          */
-
-        public abstract MyuserQuery getMyUserQuery();
+        public abstract MyuserQuery getMyuserQuery();
 
         /**
-         *  يعيد كائن لعمليات جدول الموضيع
+         * يعيد كائن لعمليات جدول الموضيع
          * @return
          */
-
-
         public abstract MySubjectQuery getMySubjectQuery();
 
         /**
-         *  يعيد كائن لعمليات جدول المهمات
+         * يعيد كائن لعمليات جدول المهمات
          * @return
          */
-
         public abstract MyTasksQuery getMyTaskQuery();
 
         /**
@@ -62,17 +56,18 @@ public abstract class AppDatabase extends RoomDatabase
          * @param context
          * @return
          */
-
-        public static AppDatabase getDB(Context context) {
-                if (db == null) {
+        public static AppDataBase getDB(Context context)
+        {
+                if(db==null)
+                {
                         db = Room.databaseBuilder(context,
-                                        AppDatabase.class, "database-name")
+                                        AppDataBase.class,
+                                        "samihDataBase")//اسم قاعدة اليانات
                                 .fallbackToDestructiveMigration()
                                 .allowMainThreadQueries()
                                 .build();
                 }
                 return db;
-
-
         }
+
 }
